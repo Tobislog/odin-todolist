758da37f0ba831a2c308bb5b287e9dccfbd96b19
Timer110 <121953691+Timer110@users.noreply.github.com>
2026-04-11 11:36:44 +0200

    added basic UI and implement ui renderer file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+import './style.css';
 import { createTask, storeTaskToDB, getTasks, debugTasks } from "./task";
+import { renderCategories } from './render';
+
+
+//Testing and Debugging
 
 const testTask = createTask("standard", "Einkaufen gehen", "Meine Beschreibung", Date("2026-06-11T11:29:00"), 2);
 const testTask2 = createTask("standard", "Code Schreiben", "Eine To-Do-App schreiben", Date("2026-06-11T08:48:00"), 1);
+const specialTask = createTask("special", "SpezialTask", "keine Ahnung", Date("2026-06-11T23:11:00", 2))
 
-storeTaskToDB(testTask);
-storeTaskToDB(testTask2);
+// storeTaskToDB(testTask);
+// storeTaskToDB(testTask2);
+// storeTaskToDB(specialTask);
 
 debugTasks();
+
+tasks.forEach(task => {
+    if (task.category === "standard") {
+        console.log("Standard-tasks: " + task.title);
+    }
+    else if (task.category === "special") {
+        console.log("Spezial-Task: " + task.title);
+    }
+});
---
+++ src/render.js
@@ -0,0 +1,8 @@
+//DOM Variables
+const $sidebar = document.querySelector(".sidebar");
+
+
+export function renderCategories () {
+    $sidebar.innerHTML = "";
+    
+}
